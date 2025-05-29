66b09bcc0a6c153ccb493423d46ce8b9161a95b7
Pietro Di Bello <pierodibello@gmail.com>
2025-05-29 08:36:40 +0200

    first exercise

--- intro/part1/Main.elm
+++ intro/part1/Main.elm
@@ -4,16 +4,14 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+banner : Html msg
 banner =
     {- 👉 TODO: Add a logo and tagline to this banner, so its structure becomes:
 
           <div class="banner">
               <div class="container">
-
                   <h1 class="logo-font">conduit</h1>
-
                   <p>A place to share your knowledge.</p>
-
                </div>
           </div>
 
@@ -23,17 +21,21 @@ banner =
     -}
     div [ class "banner" ]
         [ div [ class "container" ]
-            [ text "👉 TODO: Put the <h1> here instead of this text, then add the <p> right after the <h1>" ]
+            [ h1 [ class "logo-font" ] [ text "conduit" ]
+            , p [] [ text "A place to share your knownledge." ]
+            ]
         ]
 
 
+feed : Html msg
 feed =
-    div [ class "feed-toggle" ] [ text "(In the future we’ll display a feed of articles here!)" ]
+    div [ class "feed-toggle" ] [ text "(In the future we'll display a feed of articles here!)" ]
 
 
+main : Html msg
 main =
     div [ class "home-page" ]
-        [ div [] [ text "👉 TODO: Replace this <div> with the banner" ]
+        [ banner
         , div [ class "container page" ]
             [ div [ class "row" ]
                 [ div [ class "col-md-9" ] [ feed ]
