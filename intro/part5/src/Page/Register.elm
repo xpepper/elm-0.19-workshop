0ea4722785dc9b9933872b5561ae09f5dcaecd8f
Pietro Di Bello <pierodibello@gmail.com>
2025-06-30 08:52:00 +0200

    fix: add username input handling and update user database entries

--- intro/part5/src/Page/Register.elm
+++ intro/part5/src/Page/Register.elm
@@ -86,11 +86,7 @@ viewForm form =
             [ input
                 [ class "form-control form-control-lg"
                 , placeholder "Username"
-
-                {- 👉 TODO: when the user inputs a username, update it in the Model.
-
-                   💡 HINT: Look at how the Email input below does this. 👇
-                -}
+                , onInput EnteredUsername
                 , value form.username
                 ]
                 []
@@ -139,6 +135,7 @@ viewProblem problem =
 
 type Msg
     = SubmittedForm
+    | EnteredUsername String
     | EnteredEmail String
     | EnteredPassword String
     | CompletedRegister (Result Http.Error Viewer)
@@ -148,6 +145,8 @@ type Msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        EnteredUsername username ->
+            updateForm (\form -> { form | username = username }) model
         EnteredEmail email ->
             updateForm (\form -> { form | email = email }) model
 
